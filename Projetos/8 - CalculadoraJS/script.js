25f697106eed648eccbfd69db7af1e1135a88058
Eduardo Decarli <decarliedu@gmail.com>
2024-08-02 19:41:28 -0300

    Atualizacao de projetos

--- Projetos/8 - CalculadoraJS/script.js
+++ Projetos/8 - CalculadoraJS/script.js
@@ -1,19 +1,19 @@
-function calcular(tipo, valor){
-    if(tipo === 'acao'){
-        if( valor === 'c'){
+function calcular(tipo, valor) {
+    if (tipo === 'acao') {
+        if (valor === 'c') {
             //limpa o visor do resultado
             document.getElementById('resultado').value = 0
         }
-        //odiciona o operador ao calculo e mostra no visor
-        if(valor === '+' || valor === '-' || valor === '*' || valor === '/'|| valor === '.'){
+        //adiciona o operador ao calculo e mostra no visor
+        if (valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.') {
             document.getElementById('resultado').value += valor
         }
         //usa a função eval() para receber um calculo em string e retornar como resposta em numeral
-        if(valor === '='){
+        if (valor === '=') {
             var valor_campo = eval(document.getElementById('resultado').value)
             document.getElementById('resultado').value = valor_campo
-        }   
-    }else if(tipo === 'valor'){
+        }
+    } else if (tipo === 'valor') {
         document.getElementById('resultado').value += valor
     }
 }
---
+++ Projetos/9 - MataMosquito/script.js
@@ -0,0 +1,70 @@
+//Define o tamanho da tela
+var altura = 0;
+var largura = 0;
+var vidas = 1
+
+function ajustaTamanhoPalcoJogo() {
+    altura = window.innerHeight
+    largura = window.innerWidth
+}
+
+ajustaTamanhoPalcoJogo()
+
+function posicaoRandomica() {
+
+    //remover o mosquito anterior(caso exista)
+    if (document.getElementById('mosquito')) {
+        document.getElementById('mosquito').remove()
+        if (vidas > 3) {
+            window.location.href = 'fim_de_jogo.html'
+        } else {
+            document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png"
+            vidas++
+        }
+    }
+    //Define as posições para o mosquito
+    var posicaoX = Math.floor(Math.random() * largura) - 90;
+    var posicaoY = Math.floor(Math.random() * altura) - 90;
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX;
+    posicaoY = posicaoY < 0 ? 0 : posicaoY;
+
+    //Criar o elemento html
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosca.png'
+    mosquito.className = tamanhoAleatorio() + " " + ladoAleatorio()
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
+    mosquito.onclick = function () {
+        this.remove()
+    }
+
+    document.body.appendChild(mosquito)
+
+}
+
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3)
+
+    switch (classe) {
+        case 0:
+            return 'mosquito1'
+        case 1:
+            return 'mosquito2'
+        case 2:
+            return 'mosquito3'
+    }
+}
+
+function ladoAleatorio() {
+    var classe = Math.floor(Math.random() * 3)
+
+    switch (classe) {
+        case 0:
+            return 'ladoA'
+        case 1:
+            return 'ladoB'
+    }
+}
